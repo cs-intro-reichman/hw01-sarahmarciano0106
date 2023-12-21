2e961ff4004d58f7cd413c5075533364252f8975
Sarah Marciano <sarah.marciano@post.runi.ac.il>
2023-12-21 13:09:25 +0200

    homework 1

--- Coins.java
+++ Coins.java
@@ -5,5 +5,15 @@
 public class Coins {
 	public static void main(String[] args) {
 		// Put your code here
+		int coins = Integer.parseInt(args[0]) ;
+
+		int quarters = coins / 25 ;
+
+		int cents = coins - ( quarters * 25 ) ;
+
+
+		System.out.println( " Use " + quarters + " quarters and " + cents + " cents " ) ;
+
 	}
 }
+	
