e44bfb7128f3851c0bbc21ab85ee4426b1e79ea9
sarahmarciano0106 <154500367+sarahmarciano0106@users.noreply.github.com>
2023-12-21 13:18:56 +0200

    Update Coins.java

--- Coins.java
+++ Coins.java
@@ -1,6 +1,6 @@
 /*
  * Write a program that gets a quantity of cents as a command-line argument.
- * The program prints how to represent this quantity using as many quarters as possible, plus the remainder in cents.
+ * The program prints how to represent this quantity using as many quarters as possible, plus the remainder in cents. 
  */
 
 
