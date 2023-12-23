a4adf3a58cb9a3592a438fb7ca286f490940a5bc
sarahmarciano0106 <154500367+sarahmarciano0106@users.noreply.github.com>
2023-12-23 19:11:52 +0200

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -5,6 +5,17 @@
  */ 
 public class Triangle {
 	public static void main(String[] args) {
-		// Put your code here	
+		int a = Integer.parseInt(args[0]) ;
+		int b = Integer.parseInt(args[1]) ;
+		int c = Integer.parseInt(args[2]) ;
+
+		int sum1 = a + b ;
+		int sum2 = b + c ; 
+		int sum3 = a + c ;
+
+		boolean t = (sum1>c && sum2>a && sum3>b) ;
+
+	System.out.println(a + " , " + b + " , " + c + " : " + t ) ;
+    	
 	}
 }
